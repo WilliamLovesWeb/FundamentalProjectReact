e9fbb86bf5392994dd563fd358ca580682521a54
unknown <williammeilland1@gmail.com>
2023-12-15 14:52:57 +0100

    Menu Completed

--- menu/src/App.jsx
+++ menu/src/App.jsx
@@ -4,19 +4,41 @@ import Menu from './Menu/Menu'
 import menu from './assets/data'
 
 function App() {
+  const [listMenu,setListMenu] = useState(menu);
+
+  const all = () => {
+    setListMenu(menu);
+  }
+
+  const breakfast = () => {
+    const newList = menu.filter((food)=> food.category === 'breakfast');
+    setListMenu(newList);
+  }
+
+  const lunch = () => {
+    const newList = menu.filter((food)=> food.category === 'lunch');
+    setListMenu(newList);
+  }
+
+  const shakes = () => {
+    const newList = menu.filter((food)=> food.category === 'shakes');
+    setListMenu(newList);
+  }
 
   return (
     <div className='App'>
       <div className='title'>Our Menu</div>
       <div className='border'></div>
       <div className='search'>
-        <button className='All'>All</button>
-        <button className='Breakfast'>Breakfast</button>
-        <button className='Lunch'>Lunch</button>
-        <button className='Shakes'>Shakes</button>
+        <button className='All' onClick={all}>All</button>
+        <button className='Breakfast' onClick={breakfast}>Breakfast</button>
+        <button className='Lunch' onClick={lunch}>Lunch</button>
+        <button className='Shakes' onClick={shakes}>Shakes</button>
       </div>
       <div className="ListMenu">
-        <Menu {...menu[0]} key={menu[0].id} />
+        {listMenu.map((food)=>{
+          return <Menu {...food} key={food.id} />
+        })}
       </div>
     </div>
   )
--- menu/src/Menu/Menu.jsx
+++ menu/src/Menu/Menu.jsx
@@ -1,10 +1,22 @@
 import './Menu.css'
 const Menu = ({title,category,price,img,desc}) => {
+  function capitalizeFirstLetterEachWord(str) {
+    // Split the string into an array of words
+    const words = str.split(' ');
+
+    // Capitalize the first letter of each word
+    const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+
+    // Join the words back into a string
+    const result = capitalizedWords.join(' ');
+
+    return result;
+  }
   return (
     <div className='menu'>
         <img src={img} alt={title +' '+ category} />
-        <div className="title">{title}</div>
-        <div className="price">{price}</div>
+        <div className="title">{capitalizeFirstLetterEachWord(title)}</div>
+        <div className="price">${price}</div>
         <div className="historic">{desc}</div>
     </div>
   )
